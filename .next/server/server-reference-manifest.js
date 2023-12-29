660a7642282e737789799da6fbe53db2c41777fc
Jayson Tian <48577877+Jaysontian@users.noreply.github.com>
2023-12-29 14:14:53 -0800

    changes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {}\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {\n    \"fbbf9855c545166308585f21ba87204fc759ae69\": {\n      \"workers\": {\n        \"app/u/[uid]/page\": \"(rsc)/./node_modules/next/dist/build/webpack/loaders/next-flight-action-entry-loader.js?actions=%5B%5B%22%2FUsers%2Fjaysontian%2FGit%2Fnebulo-alpha-v1%2Fapp%2Fu%2F%5Buid%5D%2Fprivate.tsx%22%2C%5B%22default%22%5D%5D%5D&__client_imported__=!\"\n      },\n      \"layer\": {\n        \"app/u/[uid]/page\": \"rsc\"\n      }\n    }\n  },\n  \"edge\": {}\n}"
--- app/u/widgets/spotifytop.tsx
+++ app/u/widgets/spotifytop.tsx
@@ -3,6 +3,7 @@
 import useSWR from 'swr';
 import axios from 'axios';
 import "@/app/u/widgets/spotifytop.css"
+import { useState, useEffect } from 'react'
 
 import SpotifyButton from "@/app/signin/apps/spotifybutton";
 
@@ -10,16 +11,75 @@ const fetcher = ([url, token] : any) => axios
       .get(url, { headers: { Authorization: "Bearer " + token } })
       .then((res) => res.data);
 
+
+const refetch = async() => {
+    await fetch('http://localhost:3000/api/auth/spotify/refresh_token').then(res => res.json()).then(token => {
+        console.log("Fetched new token: ", props.token, token);
+        return token;
+    }).catch((err) => {
+        return(<div>Spotify did not grant access. Either you need to be added to developer beta or your refresh token expired. Contact Jayson.</div>);
+    });
+}
+
 const widgetContents = (props : any) => {
 
     if (props.token == null) return (<><h2>Your Top 5</h2><SpotifyButton></SpotifyButton></>);
-    const { data, error } = useSWR([`https://api.spotify.com/v1/me/top/tracks?limit=5`, props.token.access_token], fetcher);
 
-    if (error && error.response.status == 401){
-        fetch('http://localhost:3000/api/auth/spotify/refresh_token').then(res => res.json()).then(data => console.log('Token refresh success:', data));
-        return(<div>Spotify did not grant access. Either you need to be added to developer beta or your refresh token expired. Contact Jayson.</div>);
-    }
+    const { data, error } = useSWR([`https://api.spotify.com/v1/me/top/tracks?limit=5`, props.token.access_token], fetcher, {
+        onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
 
+            // // If not a 401 error, then return
+            if (error.response.status !== 401) return
+            // Only retry up to 2 times.
+            if (retryCount >= 2) return
+            console.log('RETRYING');
+            
+            refetch();
+            
+
+            // Retry after 5 seconds.
+            setTimeout(() => revalidate({ retryCount }), 2000)
+        }
+    });
+
+    // if (error && error.response.status == 401) {
+    //     console.log("Refetching Data");
+    //     fetch('http://localhost:3000/api/auth/spotify/refresh_token').then(res => res.json()).then(token => {
+    //         props.token = token;
+    //     }).catch((err) => {
+    //         return(<div>Spotify did not grant access. Either you need to be added to developer beta or your refresh token expired. Contact Jayson.</div>);
+    //     });
+    //     const { data, error } = useSWR([`https://api.spotify.com/v1/me/top/tracks?limit=5`, props.token.access_token], fetcher);
+    //     console.log("Refetched Data: ", data);
+    // }
+
+
+
+    // useEffect(()=>{
+    //     console.log('USEEFFECT CALLED');
+    //     if (refetch){
+    //         fetch('http://localhost:3000/api/auth/spotify/refresh_token').then(res => res.json()).then(newtoken => {
+    //             setToken(newtoken);
+    //         }).catch((err) => {
+    //             return(<div>Spotify did not grant access. Either you need to be added to developer beta or your refresh token expired. Contact Jayson.</div>);
+    //         });
+    //     }
+    // }, [refetch]);
+
+    // if (token.error == "invalid_grant"){
+    //     console.log("CALLING USEEFFECT");
+    //     setRefetch(true);
+    // };
+
+    // function App () {
+    //     const [mounted, setMounted] = useState(false)
+    //     const { data } = useSWR(mounted ? '/api/data' : null, fetchData)
+
+    //     useEffect(() => {
+    //         setMounted(true)
+    //     }, [])
+    // }
+        
     if (!data) {return (<div>Loading Top Tracks</div>)}
 
     const songs = (data.items).map((song : any, index : any) => (
